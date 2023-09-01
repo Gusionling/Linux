4e50efcf81e09166ec927eb6c4aaa61ac52aab2f
Gusionling <isdnhk@naver.com>
2023-09-01 22:50:35 +0900

    add : exec()

---
+++ process/exec.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+void test_func(void)
+{
+    printf("this is a test function %d\n", getpid());
+}
+
+
+int main(int argc, char **argv)
+{
+    //child의 pid
+    pid_t pid;
+    int exit_status;
+
+    printf("origin  process pid: %d\n", getpid());
+    pid = fork();
+    if(pid == 0){
+        //child process
+        printf("child process pid : %d\n", getpid());
+        if(execl("/bin/ls", "ls", "-al", NULL) == -1){
+            printf("execl() fail\n");
+            return -1;
+        }
+    }else if (pid >0){
+        //parent process
+        printf("parent process pid: %d, child pid %d\n", getpid(), pid);
+
+    }
+    test_func();
+    
+    pid = wait(&exit_status);
+    if(WIFEXITED(exit_status)){
+        printf("child %d returns %d\n",
+        pid,WEXITSTATUS(exit_status));
+    }else{
+        printf("child %d is not exited\n", pid);
+    }
+
+
+    return 0;
+}
